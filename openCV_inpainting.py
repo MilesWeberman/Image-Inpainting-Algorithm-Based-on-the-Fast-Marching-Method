323ff485ef2377d1254f3eac12c6b31d38cdc70c
MilesWeberman <webermanmiles@gmail.com>
2022-11-23 17:06:41 -0500

    initial commit

---
+++ openCV_inpainting.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('/Users/milesweberman/Desktop/inpainting/input_img1.png')
+mask = cv.imread('/Users/milesweberman/Desktop/inpainting/mask1.png',0)
+
+dst = cv.inpaint(img,mask,3,cv.INPAINT_TELEA)
+
+cv.imshow('dst',dst)
+cv.waitKey(0)
+cv.destroyAllWindows()
