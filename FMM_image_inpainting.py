9b1510fb75b6f46b0007228828cd05be0f389ef1
MilesWeberman <webermanmiles@gmail.com>
2022-12-09 14:15:07 -0500

    changes made to _init function

--- FMM_image_inpainting.py
+++ FMM_image_inpainting.py
@@ -1,41 +1,41 @@
 import math
 import heapq
-# from tkinter.tix import MAX
 import numpy as np
 
 KNOWN = 0
 BAND = 1
 INSIDE = 2
 
-INF = 1e6
+MAX_VALUE = 1e6
 EPS = 1e-6
 
 # initializing flags and T-values
 def _init(height, width, mask, radius):
-    distances = np.full((height,width), 0.0, dtype=float) 
-    flags = mask.astype(int) * INSIDE
+    distance_map = np.full((height,width), 0.0, dtype=float)
+    flags = np.full((height,width), 0, dtype=int)
+    flags[np.nonzero(mask)] = INSIDE  
+    # flags = mask.astype(int) * INSIDE (replace by line above, deals with case where some masked regions are not exactly 1) !!!!
     band = []
 
-    mask_Y, mask_X = mask.nonzero() # save indices of non-zero values in mask, that is the region we want to inpainting
+    mask_Y, mask_X = np.nonzero(mask) # save indices of non-zero values in mask, that is the region we want to inpaint
     for Y, X in zip(mask_Y,mask_X):
-       distances[Y,X] = INF
-       # error handling
+       # set T to a large value inside the region we want to inpaint
+       distance_map[Y,X] = MAX_VALUE
        for i in (-1,1):
            neigbs = [(Y + i, X), (Y, X+i)]
            for nb_y, nb_x in neigbs:
-                if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width:
+                if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width: # error handling
                     continue
-    
-                if flags[nb_y, nb_x] == BAND: 
+                if flags[nb_y, nb_x] == BAND: # error handling
                     continue
-                
-                if mask[nb_y, nb_x] == 0:
+                # if the neighbour of a point we want to inpaint is not in the region we want to inpaint then it is on the band (boundary), we update flag and T
+                if mask[nb_y, nb_x] == 0: 
                     flags[nb_y, nb_x] = BAND 
-                    distances[nb_y, nb_x] = 0.0
-                    heapq.heappush(band, (distances[nb_y, nb_x], nb_y, nb_x))
+                    distance_map[nb_y, nb_x] = 0.0
+                    heapq.heappush(band, (distance_map[nb_y, nb_x], nb_y, nb_x)) ##  maybe it's redundant adding distance map value at that point !!!!
     
     #Might need to compute distances in future 
-    return distances, flags, band
+    return distance_map, flags, band
 
 
 
