34b7f421f2531ce0351c769f3b06ce5a5cd4f3b7
MilesWeberman <webermanmiles@gmail.com>
2022-12-10 18:56:42 -0500

    Jonas is a better coder than miles

--- FMM_image_inpainting.py
+++ FMM_image_inpainting.py
@@ -1,6 +1,13 @@
 import math
 import heapq
 import numpy as np
+import cv2 as cv
+
+img = cv.imread('/Users/milesweberman/Desktop/inpainting/input_img.png')
+mask = cv.imread('/Users/milesweberman/Desktop/inpainting/mask.png',0)
+
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
 
 KNOWN = 0
 BAND = 1
@@ -10,60 +17,98 @@ MAX_VALUE = 1e6
 EPS = 1e-6 # TODO: idk why we need that
 
 # initializing flags and T-values
-def _init(mask, radius, height, width):
+def _init(mask, height, width):
     distance_map = np.full((height,width), 0.0, dtype=float)
     flags = np.full((height,width), 0, dtype=int)
     flags[np.nonzero(mask)] = INSIDE  
-    # TODO: flags = mask.astype(int) * INSIDE (replace by line above, deals with case where some masked regions are not exactly 1)
-    band = [] # TODO: do we need to heapify
+    band = [] 
 
     mask_Y, mask_X = np.nonzero(mask) # save indices of non-zero values in mask, that is the region we want to inpaint
     for Y, X in zip(mask_Y,mask_X):
        # set T to a large value inside the region we want to inpaint
        distance_map[Y,X] = MAX_VALUE
-       for i in (-1,1):
-           neigbs = [(Y + i, X), (Y, X + i)]
-           for nb_y, nb_x in neigbs:
-                if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width: # error handling
-                    continue
-                if flags[nb_y, nb_x] == BAND:
-                    continue
-                # if the neighbour of a point we want to inpaint is not in the region we want to inpaint then it is on the narrow band (boundary), we update flag and T
-                if mask[nb_y, nb_x] == 0: 
-                    flags[nb_y, nb_x] = BAND 
-                    distance_map[nb_y, nb_x] = 0.0
-                    heapq.heappush(band, (distance_map[nb_y, nb_x], nb_y, nb_x)) # TODO: maybe it's redundant adding distance map value at that point !!!! distance_map[nb_y, nb_x]
-    #Might need to compute distances in future 
+       for i in range(-1,2):
+           for j in range(-1,2):
+            if i == 0 and j == 0:
+                continue
+            nb_y = Y + i
+            nb_x = X + j
+            if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width: # error handling
+                continue
+            if flags[nb_y, nb_x] == BAND:
+                continue
+            # if the neighbour of a point we want to inpaint is not in the region we want to inpaint then it is on the narrow band (boundary), we update flag and T
+            # note that points with flag BAND are already inpainted
+            if mask[nb_y, nb_x] == 0: 
+                flags[nb_y, nb_x] = BAND 
+                distance_map[nb_y, nb_x] = 0.0
+                heapq.heappush(band, (distance_map[nb_y, nb_x], nb_y, nb_x)) # TODO: maybe it's redundant adding distance map value at that point !!!! distance_map[nb_y, nb_x]
+    # TODO: might need to compute distances in future 
     return distance_map, flags, band
 
-def _FMM(distance_map, flags, band, height, width):
+def _FMM(img, distance_map, flags, band, height, width, epsilon):
     while len(band) != 0:
         # extract the BAND point with the smallest T and mark it as KNOWN (step 1)
-        y,x = heapq.heappop(band) 
+        T,y,x = heapq.heappop(band) 
         flags[y,x] = KNOWN
         # iterate through the popped point's neighbours 
-        for i in (-1,1):
-                neigbs = [(y + i, x), (y, x + i)]
-                for nb_y, nb_x in neigbs:
-                    if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width: # error handling
-                        continue
-                    if flags[nb_y, nb_x] != KNOWN:
-                        if flags[nb_y, nb_x] == INSIDE: # if that point is in the region to be inpainted
-                            # march the boundary inward by adding a new point to it (step 2) and inpaint that point (step 3)
-                            flags[nb_y, nb_x] = BAND
-                            _inpaint_point(nb_y, nb_x)
-                        # propagates the value T of point at [y,x] to its neighbors (step 4)
-                        distance_map[nb_y, nb_x] = min(_solve_eikonal(...)) # TODO: figure out the right way !!!!!
-                        # (re)insert point in the heap 
-                        heapq.heappush(band, (nb_y, nb_x)) # TODO: do we need to add if not already in band ??????
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if i == 0 and j == 0:
+                    continue
+                nb_y = y + i
+                nb_x = x + j
 
-def _inpaint_point(img, distance_map, flags, y, x):
+                if nb_y < 0 or nb_y > height or nb_x < 0 or nb_x > width: # error handling
+                    continue
+                if flags[nb_y, nb_x] != KNOWN:
+                    if flags[nb_y, nb_x] == INSIDE: # if that point is in the region to be inpainted
+                        # march the boundary inward by adding a new point to it (step 2) and inpaint that point (step 3)
+                        flags[nb_y, nb_x] = BAND
+                        _inpaint_point(img, distance_map, flags, epsilon, nb_y, nb_x, height, width)
+                    
+                    # error handling
+                    if nb_y-1 >= 0 and nb_x-1 >= 0:
+                        sol1 = _solve_eikonal(nb_y-1, nb_x, nb_y, nb_x-1, height, width,distance_map, flags)
 
-    epsilon = 3 # TODO: fix value? parameter? function of unknown thickness (Telea 2.4)
+                    if nb_y-1 >= 0 and nb_x+1 < width: 
+                        sol2 = _solve_eikonal(nb_y-1, nb_x, nb_y, nb_x+1, height, width, distance_map, flags)   
+
+                    if nb_y+1 < height and nb_x-1 >= 0:
+                        sol3 = _solve_eikonal(nb_y+1, nb_x, nb_y, nb_x-1, height, width, distance_map, flags)
+
+                    if nb_y+1 < height and nb_x+1 < width:
+                        sol4 = _solve_eikonal(nb_y+1, nb_x, nb_y, nb_x+1, height, width, distance_map, flags)
+
+                    # propagates the value T of point at [y,x] to its neighbors (step 4)
+                    distance_map[nb_y, nb_x] = min(sol1, sol2, sol3, sol4) 
+                    # (re)insert point in the heap 
+                    # TODO: if that point was in band remove it from the heap before reinserting - maybe do that before updating distance_map and so it's more efficient
+                    # TODO: make sure we're adding to heap correctly (min is based of distance map)
+                    
+                    if flags[nb_y, nb_x] == BAND:
+                        counter = 0
+                        for k in band:
+                            dist, cur_y, cur_x = k
+                            if cur_y == nb_y and cur_x == nb_x:
+                                band[counter] = (distance_map[nb_y, nb_x], nb_y, nb_x)
+                                heapq.heapify(band)
+                                break
+                    else:
+                        heapq.heappush(band, (distance_map[nb_y, nb_x], nb_y, nb_x)) # TODO: do we need to add if not already in band ??????
+
+def _inpaint_point(img, distance_map, flags, epsilon, y, x, height, width):
+    # TODO: fix value? parameter? function of unknown thickness (Telea 2.4)
     # find neighbourhood of point to inpaint point
     B = []
-    # TODO: find neighbourhood: add if != INSIDE and check if in bound 
-    # TODO: how to find neighbourhood (only along x and y axes, or go in circle), also figure out if to include points on band
+    for i in range(y-epsilon,y+epsilon+1):
+        for j in range(x-epsilon,x+epsilon+1):
+            if i < 0 or i > height or j < 0 or j > width: # error handling
+                continue
+            if flags[i, j] == KNOWN:
+                # check if distance to point to inpaint is smaller or equal than epsilon 
+                if np.linalg.norm(np.array((y-i,x-j))) <= epsilon: # TODO: check if this is the right type of norm - see documentation (by default frobenius norm)
+                    B.append((i,j))
     
     # calculate gradient of T at [y,x]
     gradT = np.gradient(distance_map)
@@ -71,37 +116,34 @@ def _inpaint_point(img, distance_map, flags, y, x):
 
     # calculate gradient of image intensity
     gradI = np.gradient(img)
-    # TODO: check if it's ok to calculate here or do we need to calculate in loop? 
-    # TODO: check pseudocode for Telea : what is the if conditional statement (is it if we're actly calculating gradient using central diff)
 
     # initialize inpainting value to 0 
     numerator = 0
     denominator = 0 
 
-    for j,i in B:
+    for i,j in B:
         # calculate the weight function w
-        vector = np.array((y-j, x-i)) # vector from neighbourhood point to point to inpaint
+        vector = np.array((y-i, x-j)) # vector from neighbourhood point to point to inpaint
         norm_vector = np.linalg.norm(vector) # TODO: check if this is the right type of norm - see documentation (by default frobenius norm)
         dir = np.dot(gradT_yx,vector)/norm_vector
-        dist = 1/norm_vector^2
-        lev = 1/(1 + abs(distance_map[y,x] - distance_map[j,i]))
+        dist = 1/norm_vector**2
+        lev = 1/(1 + abs(distance_map[y,x] - distance_map[i,j]))
         w = dir * dist * lev
         # calculate inpainting value
-        gradI_ji = np.array((gradI[0][j,i], gradI[1][j,i]))
-        numerator += w * (img[j,i] + np.dot(gradI_ji,vector)) 
+        gradI_ij = np.array((gradI[0][i,j], gradI[1][i,j]))
+        numerator += w * (img[i,j] + np.dot(gradI_ij,vector)) 
         denominator += w
 
     # update inpainting value
-    img[y,x] = numerator/denominator
+    img[y,x] = 255#numerator/denominator
 
-def eikonal(y1, x1, y2, x2, height, width, T_vals, flags):
-
+def _solve_eikonal(y1, x1, y2, x2, height, width, T_vals, flags):
     #check if points in image
     if y1 < 0 or y1 >= height or x1 < 0 or x1 >= width:
-        return INF
+        return MAX_VALUE
         
     if y2 < 0 or y2 >= height or x2 < 0 or x2 >= width:
-        return INF
+        return MAX_VALUE
 
     #get flag of point 1 and point 2
     flag1 = flags[y1, x1] 
@@ -123,7 +165,7 @@ def eikonal(y1, x1, y2, x2, height, width, T_vals, flags):
                     if s >= T1 and s >= T2:
                         return s
                 
-                return INF
+                return MAX_VALUE
         else:
             #if only flag 1 = KNOWN
             T1 = T_vals[y1, x1]
@@ -135,8 +177,26 @@ def eikonal(y1, x1, y2, x2, height, width, T_vals, flags):
         return 1.0 + T2
 
     # neither pixel is known
-    return INF
+    return MAX_VALUE
 
+def inpaint(img, mask, epsilon):
+    # error handling
+    if img.shape != mask.shape:
+        raise ValueError("input image and mask are not the same size")
+    # TODO: figure out how ot work with 3 channels and where to separate grey one, look at Telea (change _inpaint_point accordingly)
+    height,width = img.shape
 
+    # initialization
+    distance_map, flags, band = _init(mask, height, width)
 
+    # solve inpainting distance
+    _FMM(img, distance_map, flags, band, height, width, epsilon)
+
+    return img
+
+im = inpaint(img, mask, 5)
+
+cv.imshow("Grayscale",im)
+cv.waitKey(0) 
+cv.destroyAllWindows()
 
